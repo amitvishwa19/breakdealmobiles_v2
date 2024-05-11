3bdd504d30e698d591a986f8ddcb7ff49ab76b0c
amitvishwa19 <amitvishwa19@gmail.com>
2024-05-11 14:11:10 +0530

    ok-2

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,21 @@
-import { contentfulClient } from "@/utils/contentfull";
+// import { contentfulClient } from "@/utils/contentfull";
 import Image from "next/image";
 
 
 
-export async function fetchProductData() {
-  const res = await contentfulClient.getEntries({ 'content_type': 'products' })
-  return res.items
-}
+// export async function fetchProductData() {
+//   const res = await contentfulClient.getEntries({ 'content_type': 'products' })
+//   return res.items
+// }
 
-export async function fetchOffers() {
-  const res = await contentfulClient.getEntries({ 'content_type': 'offerModal' })
-  return res.items
-}
+// export async function fetchOffers() {
+//   const res = await contentfulClient.getEntries({ 'content_type': 'offerModal' })
+//   return res.items
+// }
 
 
 
-export default async function Home() {
+export default function Home() {
   return (
     <main className='bg-red-400'>
 
